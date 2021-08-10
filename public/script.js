6e3f52fcd24306a40b26c2d5ce2b0370e95fb53c
Wiand1515 <marofual@gmail.com>
2021-08-10 04:55:45 -0400

    make it better

---
+++ public/script.js
@@ -0,0 +1,83 @@
+//variables del dom
+const $template = document.getElementById('template-producto').content,
+$contentBody = document.getElementById('content-body'),
+$fragment = document.createDocumentFragment();
+
+//url - produccion
+const urlProductos = 'https://bsale-test-wiand1515.herokuapp.com/api/productos';
+const urlCategorias = 'https://bsale-test-wiand1515.herokuapp.com/api/search/categorias/';
+const urlBuscarProducto = 'https://bsale-test-wiand1515.herokuapp.com/api/search/productos/'
+
+//Obtiene todos los productos
+const getProducts = async (url) => {
+   let res = await fetch(url),
+    json = await res.json();  
+        
+        json.data.forEach(el => {    
+            $template.querySelector("img").setAttribute("src", el.url_image);
+            $template.querySelector('h5').textContent = el.name;
+            $template.querySelector('span').textContent = el.price
+    
+            let $clone = document.importNode($template, true);
+            $fragment.appendChild($clone);
+        });
+        $contentBody.appendChild($fragment);
+    
+
+};
+
+//Obtiene los productos por categoria
+const getProductsCategory = async (url) => {
+    let res = await fetch(url),
+    json = await res.json();
+
+    json.data[0].products.forEach(el => {    
+        $template.querySelector("img").setAttribute("src", el.url_image);
+        $template.querySelector('h5').textContent = el.name;
+        $template.querySelector('span').textContent = el.price
+
+        let $clone = document.importNode($template, true);
+        $fragment.appendChild($clone);
+    });
+    $contentBody.appendChild($fragment);
+}
+
+
+//Cargar todos los productos a la carga del DOM
+document.addEventListener('DOMContentLoaded', () => {
+    
+    getProducts(urlProductos);
+});
+
+//Delegacion de eventos on click => para agregar a carrito y filtrar por categorias
+document.addEventListener('click', (e) => {
+    
+    //Agrega al carrito
+    if(e.target.matches('.boton-agregar')) {
+        alert('Boton agregar')
+    };
+    
+    //Filtra por categoria
+    if(e.target.matches('.botones-hilera button')) {
+       $contentBody.innerHTML = '';
+
+       if(e.target.value == 0) {
+        getProducts(urlProductos);
+       } else {
+        getProductsCategory(urlCategorias + e.target.value);
+       }
+    }
+});
+
+
+//Evento on submit => para la Search-bar
+document.addEventListener('submit', (e) => {    
+    const searchResult = document.getElementById('search-text').value;    
+    e.preventDefault();
+    $contentBody.innerHTML = '';
+    getProducts(urlBuscarProducto + searchResult);
+
+    if(searchResult === '') {
+        getProducts(urlProductos)
+    }
+});
--- src/app.js
+++ src/app.js
@@ -6,15 +6,14 @@ const cors = require('cors');
 //Settings
 const PORT = process.env.PORT || 3000;
 
+//Servir contenido estatico
+app.use(express.static('public'));
 
 //Middlewares
 app.use(express.json());
 app.use(cors());
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Hola Mundo');
-})
 app.use("/api/productos", require('./routes/productos'));
 app.use("/api/categorias", require('./routes/categorias'));
 app.use("/api/search", require('./routes/search'))
