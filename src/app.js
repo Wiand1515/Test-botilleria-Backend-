9f934325b1a05b4c21e1aaf5a1f737e068d232b9
Wiand1515 <marofual@gmail.com>
2021-08-10 04:33:44 -0400

    prueba!

--- src/app.js
+++ src/app.js
@@ -10,6 +10,9 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
+app.get('/', (req, res) => {
+    res.send('Hola Mundo');
+})
 app.use("/api/productos", require('./routes/productos'));
 app.use("/api/categorias", require('./routes/categorias'));
 app.use("/api/search", require('./routes/search'))
